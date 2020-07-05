e3116d649f37b2d46eba8691c22c13ed225326b8
Jose Tafur <j-osea-nt@hotmail.com>
2020-07-04 23:43:22 -0500

    updating transaction form

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transaction',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionComponent implements OnInit {
 
-  constructor() { }
+  transactionForm: FormGroup;
+  type = 'ingreso';
+
+  constructor(
+    private readonly fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.transactionForm = this.fb.group({
+      description: ['', Validators.required],
+      amount: ['', Validators.required],
+    })
+  }
+
+  save(): void {
+    if (this.transactionForm.invalid) { return; }
+    console.log(this.type)
   }
 
 }
